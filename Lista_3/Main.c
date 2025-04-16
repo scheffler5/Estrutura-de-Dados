8ee842c064047134b58e0a5aecd92939e514052b
scheffler5 <gabrischeffler2005@gmail.com>
2025-04-16 17:59:14 -0300

    Lista 3, finalizacao e comentarios

--- Lista_3/Main.c
+++ Lista_3/Main.c
@@ -10,6 +10,7 @@ int main(){
                          {4,"Ricardo",7.5,8.7,6.8},
                          {1,"Bianca",9.7,6.7,8.4},
                          {3,"Ana",5.7,6.1,7.4}};
+                         //Declara;ao de pilhas ( criacao )
     Pilha* p4 = cria_Pilha();
     Pilha* piii = cria_Pilha();
     Pilha* pi = cria_Pilha(); 
@@ -42,36 +43,58 @@ int main(){
     imprime_Pilha(pi);
     printf("Tamanho: %d\n\n\n\n",tamanho_Pilha(pi));
 
+    //EX 3
     transferir(pi);
+
+    // EX 4 
     int tamanho, maior ,menor ,media,t2;
+    // Input de um numero para o tamanho 
     printf("Digite um Numero para n :");
     scanf("%d",&tamanho);
+    //Aciono a Funcao numero para a pilha 2 e o seu respectivo tamanho
     numeros(pii,tamanho);
+
+    //EX 5
+    // input da pilha 3, seu tamanho
     printf("Digite um Numero para n :");
     scanf("%d",&t2);
+    //Faz o push de numeros para a pilha 3
     push3(piii,t2);
+    // Funcao que verifica se as pilhas sao iguais
     verificar(pii, piii);
+
+    //EX 7 
+    // Funcoes para verificar quais os numeros pares e impars na pilha 2
     par(pii);
     impar(pii);
 
+    //EX 6 
+    //Declaracao de variavel para conferir posteriormente se e` palindromo
     char str[100];
+    // funcao para povoar a pilha com char
     push4(p4,str);
+    //Funcao para imprimir a pilha formato char
     imprime_Pilha3(p4);
-
+    // Acionamento da fun;'ao de verifica;'ao de palindromo, return 0 para nao e return 1 para palindromo
     if (Palindromo(p4, str)) {
         printf("É um palíndromo!\n");
     } else {
         printf("Não é um palíndromo.\n");
     }
 
-
+    //EX 9
+    //Declaracao de variaveis e op;ao como -1 para nao ter nenhum erro no do while
     int opcao = -1, valor, n3;
+    // do-while
     do{
-        
+        // switch case para atribui;'ao de valor digitado pelo usuario
         switch (opcao) {
+            //fun;ao menu para mostrar as escolhas
             menu();
+            // input das escolhas
             printf("Escolha uma opção: ");
             scanf("%d", &opcao);
+            //case 1, verificar se pilha esta vazia
             case 1:
                 if (vazia(p5)) {
                     printf("A pilha está vazia.\n");
@@ -79,27 +102,33 @@ int main(){
                     printf("A pilha não está vazia.\n");
                 }
                 break;
+                //case 2 para verificar se tem coisa dentro da pilha (cheia)
             case 2:
                 cheia(p5);
                 break;
+                // Empilhamento de int na pilha 
             case 3:
                 printf("Digite quantos valores deseja empilhar: ");
                 scanf("%d", &n3);
                 empilhar(p5, n3);
                 imprime_Pilha3(p5);
                 break;
+                // Desempilhamento da pilha no formato int
             case 4:
                 printf("Digite quantos valores deseja desempilhar: ");
                 scanf("%d", &n3);
                 desempilhar(p5, n3);
                 imprime_Pilha3(p5);
                 break;
+                // mostrar o topo da funcao 
             case 5:
                 topo(p5);
                 break;
+                // case 7 para sair do loop com return 0
             case 7:
                 printf("Saindo...\n");
                 return 0;
+                // case 6 para liberar a pilha 
             case 6:
                 libera_Pilha(p5);
                 printf("Pilha Liberada!");
@@ -110,14 +139,15 @@ int main(){
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
     }while (opcao != 7);
-    
+    // libera;ao das pilhas 
     libera_Pilha(p5);
     libera_Pilha(p4);
     libera_Pilha(pii);
     libera_Pilha(piii);
     libera_Pilha(pi);
+    // system pause padrao
     system("pause");
-    
+    // return 0 
     return 0;
 }
 
--- Lista_3/Pilha.c
+++ Lista_3/Pilha.c
@@ -7,8 +7,11 @@
 //Defini��o do tipo Pilha
 struct elemento{
     struct aluno dados;
+    //Variavel da estrutura int para pilhas formato inteiros
     int num;
+    //variavel da estrutura char para pilhas formato char
     char palavra;
+    // variavel da estrutura int para pilhas formato inteiro
     int numero;
     struct elemento *prox;
 };
@@ -126,25 +129,36 @@ void imprime_Pilha3(Pilha* pi){
 }
 //EX 1 e 2
 int push2 (Pilha* pi,struct aluno a,int n){
+    // pego o tamanho da pilha que eu vou inserir
     for (int i=0; i<n;i++){
+        // if para pilha == null
         if(pi == NULL) return 0;
+        //declara;ao da funcao estrutura
         Elem* no;
+        // criacao de vetor com alocacao dinamica
         no = (Elem*)malloc(sizeof(Elem));
         if (no == NULL) return 0;
+        // insere os dados de aluno em no
         no-> dados = a;
+        // passa para o proximo no
         no->prox = (*pi);
+        // tira do vetor e joga na pilha
         *pi = no;}
     return 1;}
 //EX 1 e 2
 // eu reutilizei parte da função de remover da pilha para aqui e adicionei um for para a quantidade n
 int pop2(Pilha* pi,int n){
+    // for pegando o tamanho da pilha ser desempilhada
     for (int i=0;i<n;i++){
         if(pi == NULL)
             return 0;
         if((*pi) == NULL)
             return 0;
+        // atribui;ao de ponterio de no na pilha pi
         Elem *no = *pi;
+        // passa para o proximo no
         *pi = no->prox;
+        // libera a memoria do vetor no
         free(no);}
     return 1;}
 
@@ -189,10 +203,13 @@ void transferir(Pilha* pi){
     return;
 }
 //EX4
-
+// Nesta funcao eu verifico o menor numero, o maior e a media da pilha
 int numeros(Pilha* pii, int n){
+    // declaracao das variaveis
     int numero,maior = 0,menor = 0,soma = 0,media = 0;
+    // declaracao do elemento
     Elem* no;
+    // for para armazenar os numeros que o usuario disser na pilha, para conferir depois
     for (int i=0; i<n;i++){
         printf("Digite um numero para ser armazenado : ");
         scanf("%d",&numero);
@@ -204,18 +221,23 @@ int numeros(Pilha* pii, int n){
         *pii = no;
 
     }
-
+    //atribui o tamanho ao tamanho da pilha pii
     int tamanho = tamanho_Pilha(pii);
+    // while para ate quando o no for NULL
     while(no != NULL){
+        // Encontra o maior numero 
         if(maior < no->num){
             maior = no->num;
         }
+        //encontra o menor numero 
         if(menor > no->num){
             menor = no->num;
         }
+        // nesta soma eu somo todos os elementos da pilha e divido pelo tamanho da mesmo --- Media
         soma = soma + no->num;
         no = no->prox;
     }
+    // prints finais dos valores
     printf("\nMaior Numero: %d",maior);
     printf("\nMenor numero : %d",menor);
     media = soma/tamanho;
@@ -223,6 +245,8 @@ int numeros(Pilha* pii, int n){
     imprime_Pilha2(pii);
     return 1;
 }
+// Funcao para empilhar elementos inteiros
+// Nao vou me estender muito na explicacao pois ja foi explicado o funcionamento no EX 1 e 2
 int push3(Pilha* piii, int n) {
     int numero;
     Elem* no;
@@ -238,82 +262,111 @@ int push3(Pilha* piii, int n) {
     }
     return 1;
 }
-
+// Nesta funcao eu verifico se uma pilha eh igual a outra da seguinte forma
 int verificar(Pilha* pii, Pilha* piii){
+    //verificacao para ver se elas nao sao nulas ja parando logo no inicio
     if (pii == NULL || piii == NULL) {
         printf("Uma das pilhas é nula!\n");
         return 0;
     }
+    // chamada das variaveis da estrutura para puxar as pilhas
     Elem* no = *pii;   
     Elem* noo = *piii;
+    // ver os tamanhos das pilhas 
     int t1 = tamanho_Pilha(pii);
     int t2 = tamanho_Pilha(piii);
+    // verifica os tamanhos, se nao forem iguais ja para 
     if (t1 != t2) {
         printf("As pilhas têm tamanhos diferentes!\n");
         return 0;
     }
-
+    // impressao das pilhas apenas para testes e visualiza;ao dos usuarios
     imprime_Pilha2(piii);
     imprime_Pilha2(pii);
+    // while com if para verificar celula por celula da pilha 
     while (noo != NULL && no != NULL) {
         if (noo->num != no->num) {
             printf("As pilhas não são iguais!\n");
             return 0;
         }
+        // passa para o proximo no
         noo = noo->prox;
         no = no->prox;
     }
+    // print que sao iguais
     printf("As pilhas são iguais!\n");
     return 1;
 }
 //E6 
+//Funcao para empilhar char 
 int push4(Pilha* pi,char* str) {
+    // declara;ao de numero sendo char e as outras variaveis
     char numero;
     Elem* no;
     int i = 0;
-    do {
+    do {// do while para armazenar o valor impondo algumas condi;oes 
+        //input
         printf("Digite um numero para ser armazenado (Digite * para sair): ");
         scanf(" %c", &numero); 
+        // quando o user digitar * o programa para 
         if (numero == '*') break; 
+        // quando tiver espa;o ou ponto ele continua
         if (numero == ' ' || numero == '.') continue;
+        //verificacao de pi === null
         if (pi == NULL) return 0;
+        // criacao de vetor 
         no = (Elem*)malloc(sizeof(Elem));
         if (no == NULL) return 0;
+        // joga o valor de numero para a pilha 
         no->palavra = tolower(numero);
+        // passa para o proximo no 
         no->prox = (*pi);
+        // iguala o valor ao ponteiro da pilha 
         *pi = no;
+        // aqui ele joga o numero para dentro de um vetor str
         str[i++] = tolower(numero);
     } while (1);
+    // adiciona o ultimo valor como /0 ( enter)
     str[i] = '\0';
     return 1;
 }
+// nesta funcao eu pego o str e confiro na ordem ao contraria da pilha para ver se eh igual
 int Palindromo(Pilha* pi, char* str) {
     int i = 0;
     Elem* no = *pi;
+    //while para no ate NULL
     while (no != NULL) {
+        // if para verficar se a pilha eh igual ao vetor
         if (no->palavra != str[i]) {
             return 0; 
         }
+        // passa para o proximo no
         no = no->prox;
         i++;
     }
     return 1; 
 }
+// funcao para verificar quais numeros da pilha sao pares
 int par(Pilha* pi) {
+    // pega a pilha e atribui a estrutura ELEM
     Elem* no = *pi;
     printf("Números par na pilha:\n");
     while (no != NULL) {
+        // se o no da pilha tiver resto 0 ele eh par
         if (no->num % 2 == 0) {
             printf("%d\n", no->num);
+            // dai ele so printa os numeros que sao pares
         }
+        // passa para o proximo no
         no = no->prox;
     }
 }
-
+// aqui eh uma funcao para ver se eh impar, mesma ideia da funcao de par 
 int impar(Pilha* pi) {
     Elem* no = *pi;
     printf("Números ímpares na pilha:\n");
     while (no != NULL) {
+        // unica diferenca eh que ele printa quando tiver 0 na divisao por 2
         if (no->num % 2 != 0) {
             printf("%d\n", no->num);
         }
@@ -322,22 +375,24 @@ int impar(Pilha* pi) {
 }
 
 
-
+// nesta funcao eu verifico se a pilha esta vazia
 
 int vazia(Pilha *p) {
+    // verifica se tem ponteiro e valor, se null ele retorna 1
     if (p == NULL || *p == NULL) {
         return 1;  
     }
     return 0;
 }
-
+// eu verifico se a pilha esta com coisa dentro
 void cheia(Pilha *p) {
     Elem* no = *p;
+    // verifica se no no tem numero diferente de 0
     if(no->numero != 0){
         printf("Esta Cheia!");
     }
 }
-
+// funcao de empilhar igual do exercicio 1 e 2
 void empilhar(Pilha *p, int n) {
     int numero;
     Elem* no = *p;
@@ -354,7 +409,7 @@ void empilhar(Pilha *p, int n) {
         no = no->prox;
     }
 }
-
+// funcao de desempilhar igual do exercicio 1 e 2
 int desempilhar(Pilha *p, int n) {
     Elem *no = *p;
     for (int i=0;i<n;i++){
@@ -366,15 +421,16 @@ int desempilhar(Pilha *p, int n) {
         free(no);}
     return 1;
 }
-
+// ele printa o topo da pilha 
 void topo(Pilha *p) {
     if(p == NULL)
         return;
     Elem* no = *p;
+    // printa o primeiro valor da pilha === topo
     printf("Topo: %d\n",no->numero);
 }
 
-
+// funcao com prints do menu
 void menu() 
 { 
 printf("1-Verifica se a pilha e vazia.\n"); 
